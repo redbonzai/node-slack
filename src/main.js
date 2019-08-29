8caa3963b6e9f1d02a14706bca4fe74a60f556e5
Christian Dominguez <christian@redbonzai.com>
2019-08-28 21:03:21 -0500

    AddFirebase:  Completed the firebase setup.  The platform is now successfully connected to firebase. Installed firebase with npm. registered the app with the database, and obtained web credentials. Added web credentials into main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
+
+// Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyAkVhF2MlU3LspQep2P6B7NPUhZRU-CmcQ",
+  authDomain: "vuex-slack-emulator.firebaseapp.com",
+  databaseURL: "https://vuex-slack-emulator.firebaseio.com",
+  projectId: "vuex-slack-emulator",
+  storageBucket: "vuex-slack-emulator.appspot.com",
+  messagingSenderId: "523626561682",
+  appId: "1:523626561682:web:5a249af12ba03009"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+window.firebase = firebase
 
 new Vue({
   router,
